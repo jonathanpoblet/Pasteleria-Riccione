0c224d8ecaa1aec10d008176d2021e55bd87362e
unknown <jonathanpoblet228@gmail.com>
2023-10-02 16:32:35 -0300

    header full responsive added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import AppRouter from './router/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 function App() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 import './header.css';
 
 export default function Header() {
+  const navigate = useNavigate()
   return (
-    <div>Header</div>
+    <nav className="navbar navbar-expand-sm">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item" onClick={() => {
+              document.getElementById('navbarNav').className = 'navbar-collapse collapse';
+              navigate('../')
+            }}>
+              <Link to='../'>
+                Inicio
+              </Link>
+            </li>
+            <li className="nav-item" onClick={() => {
+              document.getElementById('navbarNav').className = 'navbar-collapse collapse';
+              navigate('/productos')
+            }}>
+              <Link to='/productos'>
+                Productos
+              </Link>
+            </li>
+            <li className="nav-item" onClick={() => {
+              document.getElementById('navbarNav').className = 'navbar-collapse collapse';
+              navigate('/nosotros')
+            }}>
+              <Link to='/nosotros'>
+                  Sobre Nosotros
+              </Link>            
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
